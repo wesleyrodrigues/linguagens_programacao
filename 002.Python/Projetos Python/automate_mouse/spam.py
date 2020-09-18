fbd1ec9d3e5dc8b79acdd2dfed47cb3fc4c31237
wesleyrodrigues <wesleyrg2000@gmail.com>
2020-09-18 19:59:29 -0400

    bugs e melhorias

--- 002.Python/Projetos Python/automate_mouse/spam.py
+++ 002.Python/Projetos Python/automate_mouse/spam.py
@@ -1,6 +1,9 @@
 import pyautogui
-for i in range(0, 100):
+l = "Spam Spam Spam." * 2
+
+for i in range(1, len(l)):
     pyautogui.moveTo(705, 694)
     pyautogui.click(pyautogui.position())
-    pyautogui.typewrite("Spam! "*3)
-    pyautogui.hotkey('enter')
+    if (not(l[i] == " ")):
+        pyautogui.typewrite(str(i)) # l[:i]
+        pyautogui.hotkey('enter')
